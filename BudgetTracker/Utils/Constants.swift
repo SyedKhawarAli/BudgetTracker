5c147242262fd64532ec64a6d942c07e52e928c3
Shah <khawarali5@gmail.com>
2024-09-27 00:50:20 +0300

    refactor: moving to constants

--- BudgetTracker/Utils/Constants.swift
+++ BudgetTracker/Utils/Constants.swift
@@ -10,3 +10,11 @@ struct Constants {
     static let maxBudgetTypeTitleLength = 15
 
 }
+
+enum Theme: String, CaseIterable, Identifiable {
+    case light = "Light"
+    case dark = "Dark"
+    case system = "System"
+    
+    var id: String { self.rawValue }
+}
--- BudgetTracker/Views/Settings/SettingsView.swift
+++ BudgetTracker/Views/Settings/SettingsView.swift
@@ -71,11 +71,3 @@ struct SettingsView: View {
         }
     }
 }
-
-enum Theme: String, CaseIterable, Identifiable {
-    case light = "Light"
-    case dark = "Dark"
-    case system = "System"
-    
-    var id: String { self.rawValue }
-}
